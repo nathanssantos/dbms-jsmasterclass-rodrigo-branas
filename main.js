4060ccad85990622fe639544c8d504ef92e8d92e
nathanssantos <nathansilvasantos@gmail.com>
2019-01-04 14:27:58 -0200

    Exercise 6

--- main.js
+++ main.js
@@ -16,7 +16,6 @@ const database = {
             data: []
         };
 
-
         columns = columns.split(", ");
 
         for (let column of columns) {
@@ -39,12 +38,37 @@ const database = {
         }
         this.tables[tableName].data.push(row)
     },
+    select(statement) {
+        const regExp = /select (.+) from ([a-z]+) (?: where (.+))?/;
+        const parsedStatement = statement.match(regExp);
+        let [, columns, tableName, whereClause] = parsedStatement;
+        columns = column.split(", ");
+        let rows = this.tables[tableName].data;
+        if (whereClause) {
+            const [columnWhere, valueWhere] = whereClause.split(" = ");
+            rows = rows.filter(function (row) {
+                return row[columnWhere] == valueWhere;
+            });
+        }
+        rows - rows.map(function (row) {
+            let selectedRow = {};
+            columns.forEach(function (column) {
+                selectedRow[column] = row[column];
+
+            })
+            return selectedRow;
+        });
+        return rows;
+    },
     execute(statement) {
         if (statement.startsWith("create table")) {
             return this.createTable(statement);
         }
         if (statement.startsWith("insert")) {
             return this.insert(statement);
+        }
+        if (statement.startsWith("select")) {
+            return this.select(statement, message);
         }
         const message = `Syntax error: "${statement}"`;
         throw new DatabaseError(statement, message);
@@ -53,10 +77,11 @@ const database = {
 
 try {
     database.execute("create table author (id number, name string, age number, city string, state string, country string)");
-    console.log(JSON.stringify(database, undefined, " "));
     database.execute("insert into author (id, name, age) values (1, Douglas Crockford, 62)");
     database.execute("insert into author (id, name, age) values (2, Linus Torvalds, 47)");
     database.execute("insert into author (id, name, age) values (3, Martin Fowler, 54)");
+    console.log(JSON.stringify(database.execute("select name, age from author"), undefined, " "));
+    console.log(JSON.stringify(database.execute("select name, age from author where id = 1"), undefined, " "));
 } catch (e) {
     console.log(e.message);
 }
